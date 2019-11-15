01553fe8267b4a204f6f96c3d3261f7eaf3dfa75
MiguelQuispe <51973145+analyzerlabs@users.noreply.github.com>
2019-11-15 09:33:51 -0500

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,9 +1,15 @@
+#include <Servo.h>
+#include <LiquidCrystal.h>
+#include "move.cpp"
 void setup(){
-
-
+    lcd.begin(16, 2);
+    lcd.setCursor(0,1);
+    lcd.print("Iris Pong");
+    lcd.setCursor(1,1);
+    lcd.print("By AnalyzerLabs");
 }
 
 void loop(){
-
     
+
 }
