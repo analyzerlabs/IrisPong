ef24ca84cb05a152b9123d7fabfc3c8d8124fbae
MiguelQuispe <51973145+analyzerlabs@users.noreply.github.com>
2019-11-22 09:49:23 -0500

    irisPong comentado

--- IrisPong.ino
+++ IrisPong.ino
@@ -6,23 +6,23 @@
 
  Lima, November 2019
 */
-#include <Servo.h>
-#include <LiquidCrystal.h>
-#include "SHCSR04.h"
-#include "variables.h"  //incluir las librerias en orden 
-#include "sensor.h"   // puesto que algunas variables que utilizan unas estan en otras
-#include "move.h"
-#include "lcd.h"
+#include <Servo.h>    //incluimos la libreria del servo aun no funciona
+#include <LiquidCrystal.h>   //incluimos la libreria de LCD16X2
+#include "SHCSR04.h"    //incluimos la libreria del sensor HCSR04 aun no funciona
+#include "variables.h"  //incluimos las variables
+#include "sensor.h"   // incluimos las funcioones para el sensor aun on funciona
+#include "move.h"   //imcluimos las funciones para el movimiento aun no funciona
+#include "lcd.h"   //incluimos las funciones generadas para la pantalla
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(interruptPin, INPUT);
-  lcdInit();
-  sensorInit(20); // Inicia la distancia de deteccion en cm
-  menuInicio();
+  Serial.begin(9600);      //iniciamos una conexcion serial para el debugeo
+  pinMode(interruptPin, INPUT);   // asignamos al pin 19 como iterrupcion para el pulsador
+  lcdInit();                       // llamamos a la funicon lcdInit creado en el otro apartado
+  sensorInit(20);                  // llamamos a la funcion sensorInit creado en el otro apartado
+  menuInicio();         //llamamos a la funcion menuInicio creado en el otro apartado
 }
 
 void loop() {
-  menuJuego();
-  game();
+  menuJuego();    //llamamos a al funcion menuJuego creado para la pantalla LCD
+  game();           //llamamos a al funcion game creado para la pantalla LCD
 }
