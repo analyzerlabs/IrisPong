5c44719d473a77ecd07c2ef40470b8916187f526
MiguelQuispe <51973145+analyzerlabs@users.noreply.github.com>
2019-11-22 09:44:46 -0500

    variables comentadas

--- variables.h
+++ variables.h
@@ -1,15 +1,15 @@
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-SHCSR04 hcsr04;
-const byte interruptPin = 19;
-boolean control = true;
-int distance=0;
-int anterior=0;
-unsigned long t=0;
-boolean flag = false;
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;   //constantes de los pines para el control de la pantalla LCD16x2
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);                //inicializacion de la pantalla lcd 16X2
+SHCSR04 hcsr04;           //Inicializacion del sensor HCSR04
+const byte interruptPin = 19;    //constante para el control de interrupciones del pulsador
+boolean control = true;     //variable asignada al pulsador
+int distance=0;      //variable para medir distancia
+int anterior=0;      //variable auxiliar de distancia
+unsigned long t=0;     //variable de tiempo
+boolean flag = false;   //variable bandera para el control del juego
 
-int pelotas = 0;
-byte heart[8] =
+int pelotas = 0;    //variable de conteo de pelotas
+byte heart[8] =     //variable para generar el caracter Corazon
 {
 0b00000,
 0b00000,
@@ -33,8 +33,7 @@ byte R1[8] =
 0b11111,
 };
 
-byte happy[8] =
-{
+byte happy[8] =   //variable para generar el caracter carita feliz
 0b00000,
 0b01010,
 0b01010,
@@ -45,7 +44,7 @@ byte happy[8] =
 0b00000,
 };
 
-byte sad[8] =
+byte sad[8] =     //variable para generar el caracter carita triste
 {
 0b00000,
 0b01010,
@@ -57,7 +56,7 @@ byte sad[8] =
 0b00000,
 };
 
-byte pong1[8] =
+byte pong1[8] =     //variable para generar el caracter pelota arriba
 {
 0b00000,
 0b00000,
@@ -69,7 +68,7 @@ byte pong1[8] =
 0b00000,
 };
 
-byte pong2[8] =
+byte pong2[8] =     //variable para generar el caracter pelota abajo
 {
 0b00000,
 0b01110,
@@ -81,7 +80,7 @@ byte pong2[8] =
 0b00000,
 };
 
-byte reloj1[8] =
+byte reloj1[8] =    //variable para generar el caracter reloj de arena lleno
 {
 0b11111,
 0b10001,
@@ -93,7 +92,7 @@ byte reloj1[8] =
 0b11111,
 };
 
-byte reloj2[8] =
+byte reloj2[8] =     //variable para generar el caracter reloj de arena vacio
 {
 0b11111,
 0b10001,
@@ -105,7 +104,8 @@ byte reloj2[8] =
 0b11111,
 };
 
-void wait(int tiempo){
+//Funcion para generar retraso de tiempo
+void wait(int tiempo){      
   unsigned long T = millis();
   while(millis()<T+tiempo){}
 }
